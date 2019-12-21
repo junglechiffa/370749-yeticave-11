0d5b2d2eb03450927675ed82308ad41b06a02225
junglechiffa <56999526+junglechiffa@users.noreply.github.com>
2019-12-21 20:05:47 +0300

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -25,14 +25,14 @@
         <nav class="user-menu">
 
             <?php 
-                if ($is_auth == "1"){
+                if (isset($_SESSION['user'])){
                     echo '<div class="user-menu__logged">
                         <p>
                     ';
-                    echo $user_name;
+                    echo $_SESSION['user']['name'];
                     echo '</p>
                         <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                        <a class="user-menu__logout" href="#">Выход</a>
+                        <a class="user-menu__logout" href="/logout.php">Выход</a>
                         </div>
                     ';
                 }
