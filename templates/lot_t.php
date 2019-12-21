e19b2216a841d62b106a408326f45e3f59a7a830
junglechiffa <56999526+junglechiffa@users.noreply.github.com>
2019-12-21 20:05:54 +0300

    Update lot_t.php

--- templates/lot_t.php
+++ templates/lot_t.php
@@ -21,6 +21,9 @@
           <p class="lot-item__description"><?= htmlspecialchars($lot['text']); ?></p>
         </div>
         <div class="lot-item__right">
+
+          <?php if(isset($_SESSION['user'])): ?>
+
           <div class="lot-item__state">
             <div 
               <?php                  
@@ -55,8 +58,10 @@
               </p>
               <button type="submit" class="button">Сделать ставку</button>
             </form>
-
           </div>
+
+          <?php endif; ?>
+          
           <div class="history">
             <h3>История ставок (<span>10</span>)</h3>
             <table class="history__list">
